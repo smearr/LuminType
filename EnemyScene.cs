da259f479124dd34d0200caa0152db7b38b52686
sameerrahmani <56892116+Sameerrahmani@users.noreply.github.com>
2022-08-08 20:36:15 -0400

    Update EnemyScene.cs

--- EnemyScene.cs
+++ EnemyScene.cs
@@ -7,17 +7,21 @@ public class EnemyScene : KinematicBody2D
 {
 	public int runSpeed = 75;
 	public Vector2 velocity;
+	public Vector2 PlayerVelocity;
 	
 	public Label Text;
 	public LineEdit Typer;
 	public KinematicBody2D Player;
 	public Area2D crystal;
+	public AnimatedSprite PlayerSprite;
 	
+		
 	Random word_from_list = new Random();
 	
 	public override void _Ready()
 	{
 		Player = GetNode<KinematicBody2D>("/root/Node2D/Player");
+		PlayerSprite = GetNode<AnimatedSprite>("/root/Node2D/Player/AnimatedSprite");
 		Text = GetNode<Label>("Text");
 		
 		Typer = GetNode<LineEdit>("/root/Node2D/Typer");
@@ -30,22 +34,29 @@ public class EnemyScene : KinematicBody2D
 
 	public override void _Process(float delta)
 	{
+		
 		velocity = (crystal.Position - Position).Normalized() * runSpeed;
 		velocity = MoveAndSlide(velocity);
 		
 		if (Typer.Text == Text.Text)
 		{
+			Player.Position = Position;
 			Enemy_Kill();
 		}
-	
 	}
 	
-	// To calculate wpm, words typed divided by time passed in minutes.
 	public void Enemy_Kill()
 	{
 		Typer.Text = "";
+		PlayerSprite.Play("attack");
 		QueueFree();
 	}
 	
-	
+	public void return_position()
+	{
+		if (Player.Position.x != 673)
+		{
+
+		}
+	}
 }
