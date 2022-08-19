a289cd4042f9f11837467fe6cf6ab5fc9f2cf8d1
sameerrahmani <56892116+Sameerrahmani@users.noreply.github.com>
2022-08-19 13:44:32 -0400

    Update EnemyScene.cs

--- EnemyScene.cs
+++ EnemyScene.cs
@@ -7,49 +7,85 @@ public class EnemyScene : KinematicBody2D
 {
 	public int runSpeed = 75;
 	public Vector2 velocity;
-	public Vector2 PlayerVelocity;
-	
+	public Vector2 playerVelocity;
+
 	public Label Text;
 	public LineEdit Typer;
-	public KinematicBody2D Player;
 	public Area2D crystal;
-	public AnimatedSprite PlayerSprite;
+	public KinematicBody2D Player;
+	public CollisionShape2D enemyCollider;
+	public Polygon2D PlayerSprite;
+	public KinematicBody2D Enemy;
+	
+	public Polygon2D EnemySprite;
+	
 	
 		
 	Random word_from_list = new Random();
+
 	
 	public override void _Ready()
 	{
-		Player = GetNode<KinematicBody2D>("/root/Node2D/Player");
-		PlayerSprite = GetNode<AnimatedSprite>("/root/Node2D/Player/AnimatedSprite");
 		Text = GetNode<Label>("Text");
-		
 		Typer = GetNode<LineEdit>("/root/Node2D/Typer");
-		crystal = GetNode<Area2D>("/root/Node2D/Crystal");
+		Player = GetNode<KinematicBody2D>("/root/Node2D/Player");
+		EnemySprite = GetNode<Polygon2D>("Polygon2D");
+		PlayerSprite = GetNode<Polygon2D>("/root/Node2D/Player/Polygon2D");
+		crystal = GetNode<Area2D>("/root/Node2D/crystal");
+		Enemy = GetNode<KinematicBody2D>(".");
+
 		
 		
 		Text.Text = Global.easy_words[word_from_list.Next(0, Global.easy_words.Length)];
+		
+
 	}
 
 
 	public override void _Process(float delta)
-	{
+	{	
+		if (Global.is_dead == false)
+		{
+			velocity = (crystal.Position - Position).Normalized() * runSpeed;
+			velocity = MoveAndSlide(velocity);
+
+		}
 		
-		velocity = (crystal.Position - Position).Normalized() * runSpeed;
-		velocity = MoveAndSlide(velocity);
+		else if (Global.is_dead == true)
+		{
+			velocity = MoveAndSlide(new Vector2(0,0));
+		}
+		
+		LookAt(crystal.Position);
+		if (Position.x > 635)
+		{
+			Enemy.Scale = new Vector2(-1, 1);
+			Text.RectScale = new Vector2(1, -1);
+		}
+		else if (Position.x < 635)
+		{
+			EnemySprite.RotationDegrees = 90;
+			
+		}
 		
 		if (Typer.Text == Text.Text)
 		{
-			Player.Position = Position;
+			Global.pos = Position;
 			Enemy_Kill();
 		}
+		
 	}
 	
 	public void Enemy_Kill()
 	{
 		Typer.Text = "";
-		PlayerSprite.Play("attack");
+		Global.is_dead = true;
+		Global.WordsTyped += 1;
 		QueueFree();
+
 	}
 	
 }
+
+
+
