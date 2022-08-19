bdf37e6879b87e7e10558880dd80366fe4a13ca5
sameerrahmani <56892116+Sameerrahmani@users.noreply.github.com>
2022-08-19 13:44:46 -0400

    Update GameScene.cs

--- GameScene.cs
+++ GameScene.cs
@@ -1,23 +1,49 @@
 using Godot;
 using System;
+using System.Linq;
 
 public class GameScene : Node2D
 {
-	public KinematicBody2D Enemy;
 	public Timer SpawnTimer;
 	public LineEdit Typer;
-	public CollisionShape2D crystalCollision;
+	public AnimatedSprite PlayerSprite;
+	public KinematicBody2D Player;
+	public KinematicBody2D Enemy;
+	
+	
+	public Label Text;
+	public Label WPM;
+	Random pos = new Random();
 	
 	public PackedScene Enemy1 = ResourceLoader.Load("res://Scenes/EnemyScene.tscn") as PackedScene;
-
+	public PackedScene DeathEffect = ResourceLoader.Load("res://Scenes/ParticleScene.tscn") as PackedScene;	
+	
+	public float elapsed_time = 0;
 
 
 	public override void _Ready()
 	{
 		SpawnTimer = GetNode<Timer>("SpawnTimer");
 		Typer = GetNode<LineEdit>("Typer");
-		crystalCollision = GetNode<CollisionShape2D>("Crystal/CollisionShape2D");
+		WPM = GetNode<Label>("WPM");
+		Player = GetNode<KinematicBody2D>("Player");
 		
+	}
+	
+	public override void _Process(float delta)
+	{
+
+		elapsed_time += delta;
+		WPM.Text = "WPM: " + ((Global.WordsTyped / (elapsed_time / 60)));
+		
+		if(Global.is_dead == true)
+		{	
+			CPUParticles2D Particle1 = (CPUParticles2D)DeathEffect.Instance();
+			AddChild(Particle1);
+			Particle1.Position = Global.pos;
+			Particle1.Emitting = true;
+			Global.is_dead = false;
+		}
 	}
 	
 	private void _on_SpawnTimer_timeout()
@@ -25,25 +51,29 @@ public class GameScene : Node2D
 		KinematicBody2D Enemies1 = (KinematicBody2D)Enemy1.Instance();
 		KinematicBody2D Enemies2 = (KinematicBody2D)Enemy1.Instance();
 		
+	
 		AddChild(Enemies1);
-		Enemies1.Position = new Vector2(0, 250);
+		Enemies1.Position = new Vector2(0, pos.Next(0, 968));
 		
 		AddChild(Enemies2);
-		Enemies2.Position = new Vector2(1333, 250);
+		Enemies2.Position = new Vector2(1270, pos.Next(0, 968));
 	}
-	
-	private void _on_Crystal_body_entered(KinematicBody2D body)
-	{
-		GameOver();
-	}
-	
-	public void GameOver()
-	{
-		GetTree().Paused = true;
-	}
-	
-	
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
